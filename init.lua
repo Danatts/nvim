a2755e125535044dd466717e5d3a3001010aaaca
Daniel Arteta <danielatts97@hotmail.com>
2024-02-21 10:42:08 -0500

    add lazyvim

---
+++ init.lua
@@ -0,0 +1,42 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  "nvim-lualine/lualine.nvim",
+  "windwp/nvim-autopairs",
+  {"akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    require("bufferline").setup {
+      options = {
+        separator_style = "slant",
+        show_buffer_close_icons = false
+      }
+    }
+  end,
+},
+"nvim-tree/nvim-web-devicons",
+{
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {}
+  end,
+},
+"lukas-reineke/indent-blankline.nvim"
+})
